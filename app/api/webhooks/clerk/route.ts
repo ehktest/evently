7d15648810f904c2b4c52328a10fadf7ef2f76d2
ehktest <ehkarabastest@gmail.com>
2024-03-18 21:39:58 +0300

    route up

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -18,10 +18,13 @@ export async function POST(req: Request) {
   }
 
   // Get the headers
-  const headerPayload = headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
+  // const headerPayload = headers();
+  // const svix_id = headerPayload.get("svix-id");
+  // const svix_timestamp = headerPayload.get("svix-timestamp");
+  // const svix_signature = headerPayload.get("svix-signature");
+  const svix_id = req.headers.get("svix-id");
+  const svix_timestamp = req.headers.get("svix-timestamp");
+  const svix_signature = req.headers.get("svix-signature");
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
@@ -32,7 +35,8 @@ export async function POST(req: Request) {
 
   // Get the body
   const payload = await req.json();
-  const body = JSON.stringify(payload);
+  // const body = JSON.stringify(payload);
+  // İşleme `payload` ile devam edin, `JSON.stringify()` kullanmanıza gerek yok.
 
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
@@ -41,7 +45,13 @@ export async function POST(req: Request) {
 
   // Verify the payload with the headers
   try {
-    evt = wh.verify(body, {
+    // evt = wh.verify(body, {
+    //   "svix-id": svix_id,
+    //   "svix-timestamp": svix_timestamp,
+    //   "svix-signature": svix_signature,
+    // }) as WebhookEvent;
+    evt = wh.verify(payload, {
+      // `payload` burada doğrudan geçirilmeli, stringify olmadan
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
